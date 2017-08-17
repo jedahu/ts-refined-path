6aee918a6662c9004cf63da71d07529ff4a0456c
Jeremy Hughes <jedahu@gmail.com>
2017-08-17 16:22:34 +1200

    reexport "path" members

--- README.md
+++ README.md
@@ -2,7 +2,15 @@ Refinements for node paths
 
 ## Tags
 
-`Path`, `Dir`, and `File` perform no checks. Use to tag after checking.
+`FPath`, `Dir`, and `File` perform no checks. Use to tag after checking.
 
 ## Refinements
 `Abs` and `Rel` check using node's `path.isAbsolute`.
+
+## Helper types
+
+`Path<A>` is aliased to `Refined<string, FPath & A>`
+
+## `path` module reexports
+
+Node `path` members have been reexported with refined types.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 // #+TITLE: Refinements for paths
 
-import {Refinement, Tagged} from "ts-refined";
 import * as p from "path";
+import {Nil} from "ts-refined";
+import {Refined} from "ts-refined";
+import {Refinement} from "ts-refined";
+import {Tagged} from "ts-refined";
 
 
 // ** Tags
 
-export class Path extends Tagged<string> {
+export class FPath extends Tagged<string> {
     "@nominal" : "de222c7d-3565-435e-9bfc-1750a7d07838";
 }
 
@@ -30,3 +33,34 @@ export class Rel implements Refinement<string> {
     "@nominal" : "5bed8e96-5562-444c-b42a-9bcb35a77448";
     test = (s : string) => !p.isAbsolute(s);
 }
+
+
+// ** Helper types
+
+export type Path<A = Nil> = Refined<string, FPath & A>;
+
+
+// ** Retyped path functions
+
+
+export const basename : <A>(s : Path<A>, ext? : string) => Path<A> = p.basename as any;
+
+export const dirname : <A>(s : Path<File & A> | Path<A>) => Path<A> = p.dirname as any;
+
+export const extname : (s : Path) => string = p.extname;
+
+export const format : (o : p.ParsedPath) => Path = p.format as any;
+
+export const isAbsolute : <A>(s : Path<A>) => s is Path<Abs & A> = p.isAbsolute as any;
+
+export const join : <A extends Abs | Rel | Nil>(s : Path<A>, ...ss : Array<Path>) => Path<A> = p.join as any;
+
+export const normalize : <A>(s : Path<A>) => Path<A> = p.normalize as any;
+
+export const parse : (s : Path) => p.ParsedPath = p.parse;
+
+export const relative : (from : Path, to : Path) => Path = p.relative as any;
+
+export const resolve : (...ss : Array<Path>) => Path<Abs> = p.resolve as any;
+
+export {delimiter, posix, sep, win32} from "path";
